71bb22ef51d3ddda9a80e40e4fec64bcd80633b4
JhonSilva98 <jhonatansilva98@outlook.com>
2023-07-23 23:00:21 -0300

    test funcao impress

--- lib/telaFinal/funcao/funcionImpressao.dart
+++ lib/telaFinal/funcao/funcionImpressao.dart
@@ -1,12 +1,22 @@
+import 'dart:io';
+
 import 'package:document_scanner_flutter/configs/configs.dart';
 import 'package:document_scanner_flutter/document_scanner_flutter.dart';
 import 'package:flutter/material.dart';
 
 class ImpressaoUp {
+  File? pdfFinal;
+  bool verificador = false;
+
   Future<void> getImageGallery(BuildContext context) async {
+    verificador = false;
     try {
-      var scannedDoc = await DocumentScannerFlutter.launch(context,
-          source: ScannerFileSource.GALLERY); // Or ScannerFileSource.GALLERY
+      var scannedDoc = await DocumentScannerFlutter.launchForPdf(context,
+          source: ScannerFileSource.GALLERY);
+      if (scannedDoc != null) {
+        pdfFinal = scannedDoc;
+        verificador = true;
+      }
       // `scannedDoc` will be the image file scanned from scanner
     } catch (error, stackTrace) {
       // 'Failed to get document path or operation cancelled!';
@@ -16,10 +26,14 @@ class ImpressaoUp {
   }
 
   Future<void> getImageCamera(BuildContext context) async {
+    verificador = false;
     try {
-      var scannedDoc = await DocumentScannerFlutter.launch(context,
-          source: ScannerFileSource.CAMERA); // Or ScannerFileSource.GALLERY
-      // `scannedDoc` will be the image file scanned from scanner
+      var scannedDoc = await DocumentScannerFlutter.launchForPdf(context,
+          source: ScannerFileSource.CAMERA);
+      if (scannedDoc != null) {
+        pdfFinal = scannedDoc;
+        verificador = true;
+      }
     } catch (error, stackTrace) {
       // 'Failed to get document path or operation cancelled!';
       print('Erro: $error');
